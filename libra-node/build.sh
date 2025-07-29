b7e4e209eb1334b720a2d22b2de2961435f072a0
David Boreham <david@bozemanpass.com>
2025-07-29 10:14:48 -0600

    Fix build script

--- libra-node/build.sh
+++ libra-node/build.sh
@@ -3,4 +3,4 @@ source ${STACK_CONTAINER_BASE_DIR}/build-base.sh
 
 FULL_REPO_PATH=${STACK_REPO_BASE_DIR}/github.com/petticur/libra-framework/
 
-docker build --target dind-rootless -t ${STACK_IMAGE_NAME}:${STACK_IMAGE_LOCAL_TAG} -f ${STACK_CONTAINER_BASE_DIR}/container/Containerfile ${build_command_args} ${STACK_CONTAINER_BASE_DIR}
+docker build --target dind-rootless -t ${STACK_IMAGE_NAME}:${STACK_IMAGE_LOCAL_TAG} -f ${FULL_REPO_PATH}/container/Containerfile ${build_command_args} ${FULL_REPO_PATH}
