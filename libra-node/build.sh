e71bbff3e5e1cc57b466a41d21aabcd3e32030bb
David Boreham <david@bozemanpass.com>
2025-08-02 14:02:52 -0600

    Update build script

--- libra-node/build.sh
+++ libra-node/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source ${STACK_CONTAINER_BASE_DIR}/build-base.sh
 
-docker build -t ${STACK_IMAGE_NAME}:${STACK_IMAGE_LOCAL_TAG} -f ${STACK_REPO_CONTAINER_DIR}/container/Containerfile ${build_command_args} ${STACK_REPO_CONTAINER_DIR}
+docker build -t ${STACK_FULL_CONTAINER_IMAGE_TAG} -f ${STACK_REPO_CONTAINER_DIR}/container/Containerfile ${build_command_args} ${STACK_REPO_CONTAINER_DIR}
